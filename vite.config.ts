8c02ebdf3a54ad0e45e7c28821b2dd90e5769e5b
art-783 <arj783spam@gmail.com>
2026-03-13 20:34:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/waste-wise-website/",
+  base: "/ww-website/",
   server: {
     host: "::",
     port: 8080,
